1b384c7137cd105b71d965823ffe3df80565199e
michael j Swallower <nickamato608@gmail.com>
2023-08-07 03:19:51 +0000

    :construction: updating colors

--- CryptoXAEA12/app.R
+++ CryptoXAEA12/app.R
@@ -11,6 +11,7 @@ library(shinycssloaders)
 library(quantmod)
 library(CandleStickPattern)
 library(shinybusy)
+library(shinythemes)
 
 riingo_set_token("6fbd6ce7c9e035489f6238bfab127fcedbe34ac2")
 
@@ -23,7 +24,13 @@ checkbox_list = setNames(str1, str1)
 
 # Define UI
 ui <- dashboardPage(
-  dashboardHeader(title = img(src='logo.nobg.png', height = 30, width = 30, position = "right"),
+  skin = "red",
+  dashboardHeader(title = shinyDashboardLogo(
+    theme = "poor_mans_flatly",
+    boldText = "Crypto Predictor",
+    mainText = img(src='fulllogo-removebg.png', width = 70, height = 70),
+    badgeText = "v1.0"
+  ),
   titleWidth = 300
   ),
   
@@ -38,36 +45,39 @@ ui <- dashboardPage(
   
   dashboardBody(
     shinyjs::useShinyjs(),
-    shinyDashboardThemes(
-      theme = "flat_red"
-    ),
+    # shinyDashboardThemes(
+    #   theme = "flat_red"
+    # ),
     tabItems(
       tabItem(tabName = "predict",
-        fluidRow(
-          img(src='logo.nobg.png', width = 100, height = 100, align = 'right' ),
+        fluidPage(
+          theme = shinytheme("cyborg"),
           add_busy_spinner(spin = "circle", color = "white", height = "100px", width="100px", position = "top-right"),
           column(width = 6,
-            box(title = "Inputs", solidHeader = TRUE, status = "danger", width = NULL,
+            box(title = "Inputs", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
                 selectInput("selectCandlePred", "Select a Coin",choices = checkbox_list),
                 selectInput("selectTimeframePred", "Select a Timeframe",choices = list("1 Hour" = "1hour",
                                                                                        "4 Hour" = "4hour",
                                                                                        "8 Hour" = "8hour",
                                                                                        "1 Day" = "1day")),
-                actionButton("predictButtonPred","Predict", icon = icon('chart-simple'), class = class("danger"), style='padding:4px; width:100%')
+                actionButton("predictButtonPred","Predict", icon = icon('chart-simple'), class = "btn-primary", style='padding:4px; width:100%')
                 
             ),
-            box(title = "Prediction", solidHeader = TRUE, status = "danger", width = NULL,
-                infoBoxOutput("Prediction", width = 12),
-                textOutput("predictionText")
+            box(title = "Prediction", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
+                valueBoxOutput("Prediction", width = 12),
+                textOutput("predictionText"),
+                textOutput("warningText")
               
             )
           ),
 
           column(width = 6,
-            box(title = "Live Candle Chart", solidHeader = TRUE, status = "danger", width = NULL,
+            box(title = "Live Candle Chart", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
                 plotlyOutput('candlestickPlotPred')
             )
-          )
+          ),
+          img(src='fulllogo-removebg3.png', width = 200, height = 200, align = 'right' )
+          
 
         )
       ),
@@ -75,14 +85,13 @@ ui <- dashboardPage(
       tabItem(tabName = "create",
               fluidRow(
                 add_busy_spinner(spin = "circle", color = "white", height = "100px", width="100px", position = "top-right"),
-                img(src='logo.nobg.png', width = 175, height = 175, align = 'right' ),
                 setBackgroundImage(
                   # color = "black",
                   src = "tower.jpg",
                   shinydashboard = TRUE
                 ),
                 column(width = 6,
-                       box(title = "Inputs", solidHeader = TRUE, status = "danger", width = NULL,
+                       box(title = "Inputs", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
                            selectInput("selectCandle", "Select a Coin",choices = checkbox_list),
                            selectInput("selectTimeframe", "Select a Timeframe",choices = list("1 Hour" = "1hour",
                                                                                               "4 Hour" = "4hour",
@@ -94,40 +103,40 @@ ui <- dashboardPage(
                            #            style = "jelly",
                            #            color = "danger",
                            #            block = TRUE),
-                           actionButton("predictButton","Predict", icon = icon('chart-simple'), class = class("danger"), style='padding:4px; width:100%')
+                           actionButton("predictButton","Predict", icon = icon('chart-simple'), class = "btn-primary", style='padding:4px; width:100%')
                        ),
-                       box(title = "Predicted High, Low, Close", solidHeader = TRUE, status = "danger", width = NULL,
+                       box(title = "Predicted High, Low, Close", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
                            
                            br(),
                            br(),
-                           infoBoxOutput("High", width = 12),
-                           infoBoxOutput("Low", width = 12),
-                           infoBoxOutput("Close", width = 12)
+                           valueBoxOutput("High", width = 12),
+                           valueBoxOutput("Low", width = 12),
+                           valueBoxOutput("Close", width = 12)
                        ),
-                       box(title = "Predicted Break High/Low", solidHeader = TRUE, status = "danger", width = NULL,
-                           infoBoxOutput("BreakHigh", width = 12),
-                           infoBoxOutput("BreakLow", width = 12)
+                       box(title = "Predicted Break High/Low", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
+                           valueBoxOutput("BreakHigh", width = 12),
+                           valueBoxOutput("BreakLow", width = 12)
                            
                        ),
-                       box(title = "Predicted 1% Increase", solidHeader = TRUE, status = "danger", width = NULL,
-                           infoBoxOutput("oneperc", width = 12)
+                       box(title = "Predicted 1% Increase", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
+                           valueBoxOutput("oneperc", width = 12)
                        )
                 ),
                 
                 column(width=6,
-                       box(title = "Live Candle Chart", solidHeader = TRUE, status = "danger", width = NULL,
+                       box(title = "Live Candle Chart", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
                            plotlyOutput('candlestickPlot')
                        )
-                )
+                ),
+                img(src='fulllogo-removebg3.png', width = 200, height = 200, align = 'right' )
               )
       ),
       tabItem(tabName = 'backtest',
               fluidRow(
                 add_busy_spinner(spin = "circle", color = "white", height = "100px", width="100px", position = "top-right"),
-                img(src='logo.nobg.png', width = 175, height = 175, align = 'right' ),
                 
                 column(width = 6,
-                box(title = "Back-Test Inputs", solidHeader = TRUE, status = "danger", width = NULL,
+                box(title = "Back-Test Inputs", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
                     selectInput("selectBack", "Select a Coin",choices = checkbox_list),
                     selectInput("selectTimeframeBack", "Select a Timeframe",choices = list("1 Hour" = "1hour",
                                                                                        "4 Hour" = "4hour",
@@ -139,30 +148,32 @@ ui <- dashboardPage(
                                                                                            "High" = "High",
                                                                                            "Low" = "Low",
                                                                                            "Close" = "Close")),
-                    actionButton("backTestButton","Predict", icon = icon('chart-simple'), class = class("danger"), style='padding:4px; width:100%')
+                    actionButton("backTestButton","Predict", icon = icon('chart-simple'), class = "btn-primary", style='padding:4px; width:100%')
                     
                     ),
-                box(title = "Histogram", solidHeader = TRUE, status = "danger", width = NULL,
+                box(title = "Histogram", solidHeader = TRUE, status = "danger", width = NULL,background = "black",
                     plotOutput("histogram")
                 )
 
                 ),
 
-                box(title = "Back-Test Metrics", solidHeader = TRUE, status = "danger",
-                    infoBoxOutput("precision", width = 12),
-                    infoBoxOutput("recall", width = 12),
-                    infoBoxOutput("f1", width = 12),
+                box(title = "Back-Test Metrics", solidHeader = TRUE, status = "danger",background = "black",
+                    valueBoxOutput("precision", width = 12),
+                    valueBoxOutput("recall", width = 12),
+                    valueBoxOutput("f1", width = 12),
                     
-                    infoBoxOutput("rmse", width = 12),
-                    infoBoxOutput("current.price", width = 12)
+                    valueBoxOutput("rmse", width = 12),
+                    valueBoxOutput("current.price", width = 12)
                     
-                )
+                ),
+                img(src='fulllogo-removebg3.png', width = 200, height = 200, align = 'right' )
               )
+
+              
         
       ),
       tabItem(tabName = "info",
-              img(src='logo.nobg.png', width = 175, height = 175, align = 'right' ),
-              box(title = "Method Used", solidHeader = TRUE, status = "danger",
+              box(title = "Method Used", solidHeader = TRUE, status = "danger",background = "black",
                   strong("Machine Learning Model Used:"),
                   "XGBoost",
                   br(),
@@ -187,7 +198,7 @@ ui <- dashboardPage(
                   "The purpose of having multiple models is to confirm buy signals. The more models that we can have agreeing with eachother,
                    the better chance we have of our prediction being true."
               ),
-              box(title = "Future Additions", solidHeader = TRUE, status = "danger",
+              box(title = "Future Additions", solidHeader = TRUE, status = "danger",background = "black",
                   strong("Further Testing and Parameter Tuning:"),
                   "All models can be improved through further testing and parameter tuning. parameter tuning is the method of 
                 adjusting model parameters and inputs to obtain optimal results.",
@@ -200,7 +211,9 @@ ui <- dashboardPage(
                   strong("Automated Trading:"),
                   "Automated trading can be set up to utilize whichever trading platform the user prefers!"
                   
-              )
+              ),
+              img(src='fulllogo-removebg3.png', width = 200, height = 200, align = 'right' )
+              
               
       )
     )
@@ -238,25 +251,25 @@ server <- function(input, output) {
     predict.target(input$selectCandle, input$selectTimeframe, "detail")
     
     
-    output$High = renderInfoBox({
-      infoBox("Predicted High", text.high, icon = icon("arrow-trend-up"), color = "red")
+    output$High = renderValueBox({
+      valueBox(subtitle = "Predicted High",value = text.high, icon = icon("arrow-trend-up"), color = "green")
     })
-    output$Low = renderInfoBox({
-      infoBox("Predicted Low", text.low, icon = icon("arrow-trend-up"), color = "red")
+    output$Low = renderValueBox({
+      valueBox(subtitle = "Predicted Low",value = text.low, icon = icon("arrow-trend-up"), color = "red")
     })
-    output$Close = renderInfoBox({
-      infoBox("Predicted Close", text.close, icon = icon("arrow-trend-up"), color = "red")
-    })
-    
-    output$BreakHigh = renderInfoBox({
-      infoBox("Confidence to Break Previous High", text.bh, icon = icon("arrow-trend-up"), color = "red")
-    })
-    output$BreakLow = renderInfoBox({
-      infoBox("Confidence to Break Previous Low", text.bl, icon = icon("arrow-trend-up"), color = "red")
+    output$Close = renderValueBox({
+      valueBox(subtitle = "Predicted Close",value = text.close, icon = icon("arrow-trend-up"), color = "blue")
     })
     
-    output$oneperc = renderInfoBox({
-      infoBox("Confidence to Break 1% Increase", text.perc1, icon = icon("arrow-trend-up"), color = "red")
+    output$BreakHigh = renderValueBox({
+      valueBox(subtitle = "Confidence to Break Previous High",value = text.bh, icon = icon("arrow-trend-up"), color = "green")
+    })
+    output$BreakLow = renderValueBox({
+      valueBox(subtitle = "Confidence to Break Previous Low",value = text.bl, icon = icon("arrow-trend-up"), color = "red")
+    })
+    
+    output$oneperc = renderValueBox({
+      valueBox(subtitle = "Confidence to Break 1% Increase",value = text.perc1, icon = icon("arrow-trend-up"), color = "green")
     })
 
     
@@ -281,20 +294,20 @@ server <- function(input, output) {
       hide("current.price")
     }
     
-    output$precision = renderInfoBox({
-      infoBox("Precision", precision, icon = icon("arrow-trend-up"), color = "red")
+    output$precision = renderValueBox({
+      valueBox(subtitle = "Precision",value = precision, icon = icon("arrow-trend-up"), color = "red")
     })
-    output$recall = renderInfoBox({
-      infoBox("recall", recall, icon = icon("arrow-trend-up"), color = "red")
+    output$recall = renderValueBox({
+      valueBox(subtitle = "recall",value = recall, icon = icon("arrow-trend-up"), color = "red")
     })
-    output$f1 = renderInfoBox({
-      infoBox("F1 Score", f1, icon = icon("arrow-trend-up"), color = "red")
+    output$f1 = renderValueBox({
+      valueBox(subtitle = "F1 Score",value = f1, icon = icon("arrow-trend-up"), color = "red")
     })
-    output$rmse = renderInfoBox({
-      infoBox("RMSE", rmse, icon = icon("arrow-trend-up"), color = "red")
+    output$rmse = renderValueBox({
+      valueBox(subtitle = "RMSE",value = rmse, icon = icon("arrow-trend-up"), color = "red")
     })
-    output$current.price = renderInfoBox({
-      infoBox("Current Price", current.price, icon = icon("arrow-trend-up"), color = "red")
+    output$current.price = renderValueBox({
+      valueBox(subtitle = "Current Price",value = current.price, icon = icon("arrow-trend-up"), color = "red")
     })
     output$histogram = renderPlot(p1)
     
@@ -307,21 +320,22 @@ server <- function(input, output) {
     MakePrediction(perc.close, perc.high, perc.low, pred.bh, pred.bl, pred.perc1, prev.high.perc, prev.low.perc)
     
     if(pred.count <= 0){
-      output$Prediction = renderInfoBox({
-        infoBox("Do Not Buy", pred.count, icon = icon("arrow-trend-up"), color = "red")
+      output$Prediction = renderValueBox({
+        valueBox(subtitle = "Do Not Buy",value = pred.count, icon = icon("arrow-trend-up"), color = "red")
       })
       output$predictionText = renderText("The models are showing that buying during this candle would be a poor choice.")
     }else if(pred.count == 1){
-      output$Prediction = renderInfoBox({
-        infoBox("Unclear Decision", pred.count, icon = icon("arrow-trend-up"), color = "yellow")
+      output$Prediction = renderValueBox({
+        valueBox(subtitle = "Unclear Decision", value = pred.count, icon = icon("arrow-trend-up"), color = "yellow")
       })
       output$predictionText = renderText("The models are giving mixed signals on if buying during this candle would be profitable.")
     }else{
-      output$Prediction = renderInfoBox({
-        infoBox("Buy Signal", pred.count, icon = icon("arrow-trend-up"), color = "green")
+      output$Prediction = renderValueBox({
+        valueBox(subtitle = "Buy Signal",value = pred.count, icon = icon("arrow-trend-up"), color = "green")
       })
       output$predictionText = renderText("The models are showing that buying at the beginning of this candle may prove proffitable.")
     }
+    # output$warningText = renderText("Please note that these predictions should be used in confluence with other indicators.")
   })
   
 }
